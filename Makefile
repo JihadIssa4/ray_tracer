020a0e18900c36b81c816ddf95612b8b052efdfe
charbel <khazencharbel@gmail.com>
2025-12-24 17:51:09 +0200

    minimal cleaning

---
+++ Makefile
@@ -0,0 +1,4 @@
+app:
+	cd minilibx-linux ; make; cd ..
+	cd libft ; make ; cd .. 
+	cc -Wall -Wextra -Werror *.c
--- main.c
+++ main.c
@@ -1,36 +1,55 @@
-#include "vec3.h"
-#include "color.h"
+#include "./minilibx-linux/mlx.h"
+#include <stdlib.h>
+#include <unistd.h>
+#include "libft/libft.h"
+
+#define WIDTH 256
+#define HEIGHT 256
 
 #include <stdio.h>
 
 int main(void)
 {
-    /* Image */
-    int image_width = 256;
-    int image_height = 256;
+    void *mlx;
+    void *win;
+    void *img;
+    char *addr;
+    int bpp, line_len, endian;
 
-    /* PPM header */
-    printf("P3\n%d %d\n255\n", image_width, image_height);
+    mlx = mlx_init();
+    win = mlx_new_window(mlx, WIDTH, HEIGHT, "Gradient");
+    img = mlx_new_image(mlx, WIDTH, HEIGHT);
+    addr = mlx_get_data_addr(img, &bpp, &line_len, &endian);
 
-    /* Render */
-    for (int j = 0; j < image_height; j++)
+    for (int y = 0; y < HEIGHT; y++)
     {
-        /* Progress indicator (stderr, same role as std::clog) */
-        fprintf(stderr, "\rScanlines remaining: %d ", image_height - j);
-        fflush(stderr);
+        fprintf(stderr, "\rScanlines remaining: %d ", HEIGHT - y);
+        //write(2, "haha", 4);
+        int num = HEIGHT - y;
+        char *str = ft_itoa(num);
+        int len = ft_strlen(str);
+        write(2, str, len);
+        fflush(stdout);
 
-        for (int i = 0; i < image_width; i++)
+        for (int x = 0; x < WIDTH; x++)
         {
-            color pixel_color = vec3_create(
-                (double)i / (image_width - 1),
-                (double)j / (image_height - 1),
-                0.0
-            );
+            double r = (double)x / (WIDTH - 1);
+            double g = (double)y / (HEIGHT - 1);
+            double b = 0.0;
 
-            write_color(stdout, pixel_color);
+            int ir = (int)(255 * r); //255.999
+            int ig = (int)(255 * g);
+            int ib = (int)(255 * b);
+
+            int color = (ir << 16) | (ig << 8) | ib;
+            *(unsigned int *)(addr + y * line_len + x * (bpp / 8)) = color;
         }
     }
+    printf("%s\n", addr);
+    fprintf(stderr, "\rDone.\n");
 
-    fprintf(stderr, "\rDone.                 \n");
+    mlx_put_image_to_window(mlx, win, img, 0, 0);
+    mlx_loop(mlx);
     return 0;
 }
+
